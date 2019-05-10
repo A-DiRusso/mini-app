acd9bdbc67a174a4177b117055aa967d40f82c98
Jonathan Ray <jonathanray17@gmail.com>
2019-05-10 11:32:32 -0400

    finalizes randomizer function for development

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,3 +1,27 @@
 export function randomAttributes(){
-    
+    const results = {
+        sentiments: [],
+        emotions: []
+    };
+    const emotionClassifiers = [
+        "Anger",
+        "Disgust/Dislike",
+        "Fear",
+        "Joy/Like",
+        "Sadness",
+        "Suprise",
+        "Excitement",
+        "Angst",
+        "No Emotion"
+    ];
+    const sentimentClassifiers = [
+        "Negative",
+        "Postive",
+        "Neutral"
+    ];
+    const indexOfEmotion = Math.floor(Math.random() * Math.floor(emotionClassifiers.length));
+    results.emotions = emotionClassifiers[indexOfEmotion];
+    const indexOfSentiment = Math.floor(Math.random() * Math.floor(sentimentClassifiers.length));
+    results.sentiments = sentimentClassifiers[indexOfSentiment];
+    return results;
 }
